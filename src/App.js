88e697e3a2b589f15b6f7ba0fc6ba875108454cb
Howard Kim <howardkim820@gmail.com>
2020-01-10 15:56:08 -0800

    working on lifting state up for the sibling components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import LetterContainer from "./components/LetterContainer";
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount = () => {
+
+  }
+  getDataFromImageContainer = (...args) => {
+    console.log('args ', args);
+    this.setState({
+      data: args[0]
+    })
+  }
   render() {
+    console.log('app state ', this.state);
     let alphabet = 'abcdefghijklmnopqrstuvwxyz';
     return (
       <div>
         <h1>Hangmon</h1>
-        <ImageContainer />
+        <ImageContainer getDataFromImageContainer={this.getDataFromImageContainer} />
         <div className="letter-container">
-          { alphabet.split("").map((letter, index) => {
+          {alphabet.split("").map((letter, index) => {
             return <LetterContainer key={letter} letter={letter} />;
-          }) }
+          })}
         </div>
       </div>
     );
--- src/components/ImageContainer/index.js
+++ src/components/ImageContainer/index.js
@@ -2,24 +2,58 @@ import React, { Component } from 'react'
 import pokeball from '../../images/pokeball.png'
 import lost from '../../images/lost.jpg'
 import won from '../../images/won.jpg'
+import axios from 'axios';
+
 
 export default class ImageContainer extends Component {
     constructor(props){
         super(props);
         this.state = {
-            imageSource: pokeball
+            imageSource: pokeball,
+            loading: false,
+            gameStarted: false
         }
     }
     componentDidMount = () => {
 
     }
     handleClick = () => {
-
+        this.setState({loading: true})
+        let randomNum = Math.floor(Math.random() * 151) + 1
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then((resp) => {
+            let { sprites, name } = resp.data;
+            let { front_shiny: image } = sprites;
+            let guessArray = [];
+            let nameLength = name.length;
+            for(let i = 0; i < nameLength; i++){
+                guessArray.push('_')
+            }
+            this.setState({
+              loading: false,
+              name,
+              image,
+              imageSource: image,
+              gameStarted: true,
+              guessArray
+            });
+            this.props.getDataFromImageContainer(this.state)
+        })
     }
     render() {
+        console.log('this.state ', this.state);
         return (
             <div className="image-container">
-                <img src={this.state.imageSource} />
+                <img onClick={!this.state.gameStarted ? this.handleClick : ''} src={this.state.imageSource} />
+                <div className="guess-container">
+                {this.state.guessArray && this.state.guessArray.length > 0 ? this.state.guessArray.map((guess) => {
+                    return (
+                        <div className="guess-box">
+                            <p>{guess}</p>
+                        </div>
+                    )
+                }) : ''}
+                </div>
+
             </div>
         )
     }
